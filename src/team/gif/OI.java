2c3d3a4bc21a6e4ac0ee443684b3c7e014576117
Patrick <pubelhor@gmail.com>
2015-01-28 20:31:35 -0600

    Update OI.java

--- src/team/gif/OI.java
+++ src/team/gif/OI.java
@@ -8,33 +8,31 @@ import team.gif.commands.*;
 
 public class OI {
     
-    public static final Joystick
-        leftStick = new Joystick(1),
-        rightStick = new Joystick(2),
-        auxStick = new Joystick(3);
+    public static final Joystick leftStick = new Joystick(1);
+    public static final rightStick = new Joystick(2);
+    public static final auxStick = new Joystick(3);
     
-    private final Button
-        leftTrigger = new JoystickButton(leftStick, 1),
-        aux2 = new JoystickButton(rightStick, 2),
-        aux3 = new JoystickButton(rightStick, 3),
-        aux6 = new JoystickButton(rightStick, 6),
-        aux7 = new JoystickButton(rightStick, 7);
+    private final Button leftTrigger = new JoystickButton(leftStick, 1);
+    private final Button right2 = new JoystickButton(rightStick, 2);
+    private final Button right3 = new JoystickButton(rightStick, 3);
+    private final Button right6 = new JoystickButton(rightStick, 6);
+    private final Button right7 = new JoystickButton(rightStick, 7);
     
     public static final Button auxTrigger = new JoystickButton(rightStick, 1);
     
     public OI() {
         leftTrigger.whileHeld(new ShifterHigh());
         
-        aux2.whileHeld(new CollectorReceive());
-        aux2.whenPressed(new EarsOpen());
+        right2.whileHeld(new CollectorReceive());
+        right2.whenPressed(new EarsOpen());
         
-        aux3.whileHeld(new CollectorPass());
-        aux3.whenPressed(new EarsOpen());
-        aux3.whenReleased(new CollectorStandby());
-        aux3.whenReleased(new EarsClosed());
+        right3.whileHeld(new CollectorPass());
+        right3.whenPressed(new EarsOpen());
+        right3.whenReleased(new CollectorStandby());
+        right3.whenReleased(new EarsClosed());
         
-        aux6.whileHeld(new BumperUp());
-        aux7.whileHeld(new CollectorRaise());
+        right6.whileHeld(new BumperUp());
+        right7.whileHeld(new CollectorRaise());
     }
 }
 
